f62710d5536fa2a5d987963944088bd5b8aa17f0
videoCat <30525297+videoCat@users.noreply.github.com>
2020-01-15 21:47:26 +0400

    Code refactoring

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,9 @@
-def go_to_login(browser):
-    link = browser.find_element_by_css_selector("#login_link")
-    link.click()
+from selenium.webdriver import Remote as RemoteWebDriver
+from pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    go_to_login(browser)
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
