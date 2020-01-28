ac04f7fbe436ac3029082aae63fd16a447175613
videoCat <30525297+videoCat@users.noreply.github.com>
2020-01-29 02:49:14 +0400

    Update after: 'Задание: добавление в корзину со страницы товара'

--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,7 @@ from selenium.webdriver import Remote as RemoteWebDriver
 from pages.product_page import ProductPage
 import time
 
-def test_product_page_add_to_basket(browser):
+def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     product_page = ProductPage(browser,link)
     product_page.open()
